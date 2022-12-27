2d1cf77a1b7da9c7a902705fde4e8c939ed19348
Luca <s296480@studenti.polito.it>
2022-12-27 11:55:52 +0100

    Define the class "Connection"

---
+++ core/Connection.py
@@ -0,0 +1,41 @@
+class Connection:
+    def __init__(self, given_data):
+        self._input = given_data['input']     # must be a string, so a list of one element is defined -> input
+        self._output = given_data['output']   # string, same as above -> output
+        self._signal_power = given_data['signal_power']   # same but float -> signal_power
+        self._latency = 0   # float
+        self._snr = 0       # float
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def output(self):
+        return self._output
+
+    @property
+    def signal_power(self):
+        return self._signal_power
+
+    @property
+    def latency(self):
+        return self._latency
+
+    @property
+    def snr(self):
+        return self._snr
+
+    # -------------------------------------------------------
+
+    @signal_power.setter
+    def signal_power(self, value):
+        self._signal_power = value
+
+    @latency.setter
+    def latency(self, value):
+        self._latency = value
+
+    @snr.setter
+    def snr(self, value):
+        self._snr = value
