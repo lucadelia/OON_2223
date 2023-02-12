ee96a759e5207a1cf121e4dfd799d9ee7f66c41a
lucadelia <s296480@studenti.polito.it>
2023-02-12 18:02:21 +0100

    Minor changes "Lightpath" and "Constant_definition"

--- core/Constant_definition.py
+++ core/Constant_definition.py
@@ -1,5 +1,4 @@
 from scipy.constants import h       # Plank constant [J*s]
-from scipy.constants import e
 from scipy.constants import pi
 from scipy import special
 # Numpy function, is a library that contain the function "erfcinv" -> inverse of the complementary error function
@@ -7,9 +6,9 @@ import numpy as np
 
 N_channel = 10
 
-Bn = 12.5e9             # Noise bandwidth [Hz]
+bn = 12.5e9             # Noise bandwidth [Hz]
 BERt = 1e-3             # Bit error rate (BER)
-Rs = 32e9               # Symbol rate [baud/s][Hz] -> [GHz]
+rs = 32e9               # Symbol rate [baud/s][Hz] -> [GHz]
 df = 50e9               # frequency spacing between two consecutive channels [Hz] -> [GHz]
 
 amp_gain = 16           # Gain of the amplifier [dB]
@@ -19,9 +18,10 @@ freq = 193.414e12       # frequency centered in C-band [Hz]
 alpha = 0.2             # [dB/km]
 beta2 = 2.13e-26        # [ps^2/km]
 gamma = 1.27e-3         # [m*W^-1]
+e = np.exp(1)
 
 # Functions
-alpha_lin = alpha/(10*np.log10(e))
+alpha_lin = alpha*1e-3/(20*np.log10(e))
 nf = 10 ** (amp_nf / 10)
 gain = 10 ** (amp_gain / 10)
 l_eff = 1/2*alpha_lin
--- core/Lightpath.py
+++ core/Lightpath.py
@@ -9,13 +9,31 @@ class Lightpath(SignalInformation):
     def __init__(self, signal_power, path, channel):
         SignalInformation.__init__(self, signal_power, path)
         self._channel = channel
-        self._Rs = Rs
+        self._rs = rs
         self._df = df
 
     @property
     def channel(self):
         return self._channel
 
+    @property
+    def rs(self):
+        return self._rs
+
+    @property
+    def df(self):
+        return self._df
+
+    # SETTER -----------------------------------------------------------------------------------------------------------
+
     @channel.setter
     def channel(self, value):
         self._channel = value
+
+    @rs.setter
+    def rs(self, value):
+        self._rs = value
+
+    @df.setter
+    def df(self, value):
+        self._df = value
