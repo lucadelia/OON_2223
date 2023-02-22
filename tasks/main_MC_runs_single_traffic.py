3e29579ed500ac7fc8c4622c1edd89b88671c488
lucadelia <s296480@studenti.polito.it>
2023-02-22 23:27:00 +0100

    Minor changes

--- tasks/main_MC_runs_single_traffic.py
+++ tasks/main_MC_runs_single_traffic.py
@@ -13,7 +13,7 @@ net.weighted_paths_dataframe()
 net.route_space_dataframe()
 # net.draw()
 
-transceiver = "fixed_rate"      # CHANGE IN THE NETWORK !!!!!!!!!!!
+transceiver = "fixed_rate"      # Remember to change in the "Network"
 # transceiver = "flex_rate"
 # transceiver = "shannon"
 
@@ -22,7 +22,7 @@ M = 3       # FIXED_RATE
 # M = 38    # SHANNON
 
 MC = 0          # index
-MC_runs = 20    # Number of Monte Carlo runs -> AT LEAST 20!
+MC_runs = 30    # Number of Monte Carlo runs
 conn_list = []
 n_nodes = len(net.nodes)
 traffic_matrix = np.zeros((n_nodes, n_nodes))
@@ -52,22 +52,16 @@ for MC in range(MC_runs):
         list_conn_list8.append(get_conn_rejected(conn_list))            # num of rejected
         net.reset_network()
     traffic_matrix = np.zeros((n_nodes, n_nodes))
-        # M += 1
 
         # Increments M until it can propagate all connections. When it succeeds in instantiating them all,
         # it stops increasing, the matrix becomes null and exits the cycle.
         # FULL_NETWORK (same thing for NOT_FULL but change the TOTAL_CAPACITY)
 
-# net.route_space.to_csv("../results/MC_method1/"+network_type+"/"+transceiver+"/route_space_snr.csv")  # ONLY ONE TIME! THEN COMMENT THESE LINES!
+# net.route_space.to_csv("../results/MC_method1/"+network_type+"/"+transceiver+"/route_space_snr.csv")
 # net.weighted_path.to_csv("../results/MC_method1/"+network_type+"/"+transceiver+"/weighted_path.csv")
 
-# print("Maximum bit rate: " + str(get_max_bit_rate(conn_list) / 1e9) + " Gbps")
-# print("Minimum bit rate: " + str(get_min_bit_rate(conn_list) / 1e9) + " Gbps")
-# print("Average bit rate: " + str(get_average_bit_rate(conn_list) / 1e9) + " Gbps")
-# print("Total capacity: " + str(get_total_capacity(conn_list) / 1e9) + " Gbps")
-
 plot_distribution_values(list_conn_list1, "Average bit_rate [Gbps]", "Bit_rate distribution", "../results/MC_method1/"+network_type+"/"+transceiver+"/average_bit_rate_MC.png")
-print("Average bit-rate for all the MC simulation: " + str(mean(list_conn_list1)))        # CAN BE USEFUL TO FIND THE MEAN VALUE OF ALL OCCURRENCES FIND ABOVE
+print("Average bit-rate for all the MC simulation: " + str(mean(list_conn_list1)))
 plot_distribution_values(list_conn_list2, "Total capacity requested", "Capacity distribution", "../results/MC_method1/"+network_type+"/"+transceiver+"/capacity_distribution_MC.png")
 print("Average total capacitance necessary for all the connections: " + str(mean(list_conn_list2)))
 plot_distribution_values(list_conn_list3, "Average MAXIMUM bit_rate [Gbps]", "MAXIMUM Bit_rate distribution", "../results/MC_method1/"+network_type+"/"+transceiver+"/average_MAX_bit_rate_MC.png")
@@ -79,7 +73,6 @@ print("Average MAXIMUM latency for all the MC simulation: " + str(mean(list_conn
 # plot_distribution_values(list_conn_list7, "Average MINIMUM latency [s]", "MINIMUM Latency distribution", "../results/MC_method1/"+network_type+"/"+transceiver+"/average_MIN_latency_MC.png")
 # print("Average MINIMUM latency for all the MC simulation: " + str(mean(list_conn_list7)))
 
-# REMEMBER TO CHANGE IN THE FUNCTION THE NUMBER OF THE "BINS"
 list_nsr_MC = []
 i = 0
 for i in list_conn_list5:
@@ -96,5 +89,3 @@ for j in list_conn_list8:
 plot_rejected(list_rejected_MC, "Number of rejected connections", "Rejected connection for MC runs", "../results/MC_method1/"+network_type+"/"+transceiver+"/rejected_conn_MC.png")
 print("Average number of rejected connections: " + str(mean(list_rejected_MC)))
 
-# plot_distribution(conn_list, "bit_rate", "../results/MC_method1/"+network_type+"/"+transceiver+"/single_MC_run/bit_rate_distribution.png")
-# plot_distribution(conn_list, "latency", "../results/MC_method1/"+network_type+"/"+transceiver+"/single_MC_run/latency_distribution.png")
